d9a055dd9cec49c1ea0979dcc79695bd7bde7a3f
qPalee <leealabaster04@yahoo.com>
2023-11-02 15:59:08 +0000

    removed *.sh + *.txt

--- segfault.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-#globals
-PORT=2223
-
-./client localhost $PORT a 123.45.67.89 3074
-./client localhost $PORT a 123.45.67.89 3075
-./client localhost $PORT c 123.45.67.89 3074
-./client localhost $PORT l
